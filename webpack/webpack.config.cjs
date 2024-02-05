5006730f8c173f43341efa8590197e77ba608048
arploit <arpesh_11@hotmail.com>
2024-02-06 02:15:59 +0530

    App is up and running on port 7070

---
+++ webpack/webpack.config.cjs
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+        {
+            test: /\.(js|jsx|tsx|ts)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+           }
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "../dist"),
+  },
+
+  devServer: {
+    port: 7070,
+    liveReload: true,
+  },
+};
