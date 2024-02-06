e54ee1b0f276217fa8e8e2254e4a4300b65b8320
arploit <arpesh_11@hotmail.com>
2024-02-06 23:47:16 +0530

    Added Htmlwebpack plugin

--- webpack/webpack.config.cjs
+++ webpack/webpack.config.cjs
@@ -1,15 +1,24 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { template } = require("@babel/core");
 
 module.exports = {
   entry: "./src/index.tsx",
+  mode:"development",
   devtool: "inline-source-map",
   module: {
     rules: [
+
         {
             test: /\.(js|jsx|tsx|ts)$/,
             exclude: /node_modules/,
             loader: 'babel-loader'
-           }
+           }, { 
+            test: /\.(ts|tsx)$/, 
+            loader: "ts-loader" 
+          }
+
+
     ],
   },
   resolve: {
@@ -24,4 +33,8 @@ module.exports = {
     port: 7070,
     liveReload: true,
   },
+
+  plugins:[new HtmlWebpackPlugin({
+    template:path.resolve(__dirname, "../public/index.html")
+  })]
 };
